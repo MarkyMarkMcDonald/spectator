58c2f770834325296a789f7250f1fa77616315be
Mark McDonald <mark.mcdonald.ga@gmail.com>
2013-08-06 00:35:32 -0400

    added movement of cards to record

--- client/js/recordingGame.js
+++ client/js/recordingGame.js
@@ -20,3 +20,33 @@ Template.recordingGame.events({
     Games.update({_id: game._id},{$push: pushObject});
   }
 });
+
+Template.board.rendered = function(){
+  $('.board .cards').sortable({
+    connectWith: '.cards',
+    dropOnEmpty: true
+  });
+
+  $('.board').on('sortreceive', function(event, ui){
+    var senderName = ui.sender.attr('data-name');
+    var receiverName = ui.item.parent('.cards').attr('data-name');
+    var cardName = ui.item.attr('data-name');
+
+    var game = findCurrentGame();
+    var currentPlayer = Session.get('currentPlayer');
+
+    var sendingObject = {};
+    var sendingTarget = currentPlayer + '.zones.' + senderName + '.cards';
+    sendingObject[sendingTarget] = {name: cardName};
+
+    var receivingObject = {};
+    var receivingTarget = currentPlayer + '.zones.' + receiverName+ '.cards';
+    receivingObject[receivingTarget] = {name: cardName};
+
+    Games.update({_id: game._id},{$pull: sendingObject}, function(error){
+      if (!error) {
+        Games.update({_id: game._id},{$push: receivingObject});
+      }
+    });
+  })
+};
